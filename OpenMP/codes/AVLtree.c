9180103a2f9b189fdceaf15960e117dad215d108
luca <luca.tornatore@inaf.it>
2024-10-29 09:13:49 +0100

    examples on task usage with linked lists; commit of AVLtree base code

---
+++ OpenMP/codes/AVLtree.c
@@ -0,0 +1,368 @@
+
+
+#if defined(__STDC__)
+#  if (__STDC_VERSION__ >= 199901L)
+#     define _XOPEN_SOURCE 700
+#  endif
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+
+// ······················································
+// type definition
+//
+
+// define the node type
+typedef struct nodet
+{
+  int key, depth;
+  struct nodet *left;
+  struct nodet *right;
+} node_t;
+
+
+
+// ······················································
+//  functions prototypes
+//
+
+inline int      max          ( int, int );
+inline int      get_depth    ( node_t * );
+inline int      get_imbalance( node_t * );
+inline node_t * make_node    ( int );
+
+       void     insert             (node_t ** , int);
+       node_t * insert_recursive   (node_t *, int);
+       void     traverse_pre_order (node_t *);
+       void     traverse_in_order  (node_t *);
+       void     traverse_post_order(node_t *);
+
+
+// ······················································
+//  functions definitions
+//
+
+// »»»»»»»»»»»»»»»»»»
+// helper routines
+
+int max(int x, int y)
+{ return (x > y)? x : y; }
+
+
+int get_depth( node_t *node )
+{ return ( node == NULL ? 0 : node->depth); }
+
+
+int get_imbalance( node_t *node )
+{ return ( node == NULL ? 0 : get_depth(node->left) - get_depth(node->right) ); }
+
+
+node_t * make_node(int key)
+{
+  node_t *node = (node_t *)malloc(sizeof(node_t));
+  node->key   = key;
+  node->depth = 1;
+  node->left   = NULL;
+  node->right  = NULL;
+  return(node);
+}
+
+// »»»»»»»»»»»»»»»»»»
+//  rotation routines
+
+node_t * Rrotate( node_t *node )
+/*
+ *      (node)        (nl)
+         /            /  \
+       (nl)    >>  (nlr) (node)
+         \
+	(nlr)
+ */
+{
+    node_t *nl = node->left;
+    node_t *nlr = nl->right;
+
+    // Perform rotation
+    nl->right = node;
+    node->left = nlr;
+
+    // Update heights
+    node->depth = max(get_depth(node->left),
+		      get_depth(node->right)) + 1;
+    nl->depth   = max(get_depth(nl->left),
+		      get_depth(nl->right)) + 1;
+
+    // Return new root
+    return nl;
+}
+
+node_t * Lrotate( node_t *node )
+/*
+       (node)           (nr)
+          \             /  \  
+	  (nr)  >>  (node) (nrl)
+	  /
+       (nrl)
+ */
+{
+    node_t *nr  = node->right;
+    node_t *nrl = nr->left;
+
+    // Perform rotation
+    nr->left = node;
+    node->right = nrl;
+
+    //  Update heights
+    node->depth = max(get_depth(node->left),   
+		       get_depth(node->right)) + 1;
+    nr->depth   = max(get_depth(nr->left),
+		      get_depth(nr->right)) + 1;
+
+    // Return new root
+    return nr;
+}
+
+
+// »»»»»»»»»»»»»»»»»»
+//  insertion
+
+void insert(node_t ** root, int key)
+{
+  typedef struct
+  { node_t *ancestor, **branch; } stack_t;
+      
+  node_t *new = make_node(key);
+  if (*root == NULL) {
+    *root = new;
+    return; }
+
+  // As first, we find the insertion point
+  // for a new leaf
+  //
+  
+  int      depth = (*root)->depth;
+  stack_t  stack[depth];
+  node_t  *ptr = *root;
+  int      idx = -1;
+  
+  while( ptr != NULL )
+    {
+      stack[++idx].ancestor = ptr;
+      if ( key < ptr->key ) {
+	stack[idx].branch = &(ptr->left);
+	ptr = ptr->left; }
+      else {
+	stack[idx].branch = &(ptr->right);
+	ptr = ptr->right; }
+    }
+
+  // Second, walk up the stack of
+  // ancestors and check the imbalance
+  //
+  ptr = new;  
+  while ( idx > -1 )
+    {
+      *(stack[idx].branch) = ptr;
+      
+      node_t *node = stack[idx].ancestor;
+      node->depth = 1 + max(get_depth(node->left), get_depth(node->right));
+      int imbalance = get_imbalance(node);
+
+      if ( (imbalance > 1) && (imbalance < -1) )
+	{
+	
+	  // Left Left Case
+	  if ( imbalance > 1 && key < (node->left->key) )
+	    ptr = Rrotate(node);
+	  
+	  // Right Right Case
+	  if (imbalance < -1 && key > (node->right->key) )
+	    ptr = Lrotate(node);
+	  
+	  // Left Right Case
+	  if (imbalance > 1 && key > (node->left->key) )
+	    {
+	      node->left =  Lrotate(node->left);
+	      ptr = Rrotate(node);
+	    }
+
+	  // Right Left Case
+	  if (imbalance < -1 && key < (node->right->key) )
+	    {
+	      node->right = Rrotate(node->right);
+	      ptr = Lrotate(node);
+	    }
+	}
+      else
+	ptr = node;
+      
+      idx--;
+    }
+
+  return;
+}
+
+
+node_t * insert_recursive(node_t * node, int key)
+{
+
+  // As first, we insert the node as a new leaf
+  //
+  if (node == NULL)
+    // the bottom of the tree has been reached.
+    // let's create a node and insert it here;
+    // then we'll walk back
+    return  make_node(key);
+  
+  if (key < node->key)  
+    node->left  = insert_recursive(node->left, key);
+  else if (key > node->key)
+    node->right = insert_recursive(node->right, key);
+  else
+    return node;
+
+  // Second, let's update the immediate ancestor
+  // and find its new imbalance
+    
+  node->depth = 1 + max(get_depth(node->left), get_depth(node->right));
+  int imbalance = get_imbalance(node);
+
+  if ( (imbalance <= 1) && (imbalance >= -1) )
+    return node;
+  
+  // Third, if the current node is imbalanced
+  // we undergo appropriate adjustements
+  
+  // Left Left Case
+  if ( imbalance > 1 && key < (node->left->key) )
+    return Rrotate(node);
+
+  // Right Right Case
+  if (imbalance < -1 && key > (node->right->key) )
+    return Lrotate(node);
+
+  // Left Right Case
+  if (imbalance > 1 && key > (node->left->key) )
+    {
+      node->left =  Lrotate(node->left);
+      return Rrotate(node);
+    }
+
+  // Right Left Case
+  if (imbalance < -1 && key < (node->right->key) )
+    {
+      node->right = Rrotate(node->right);
+      return Lrotate(node);
+    }
+
+}
+
+
+// »»»»»»»»»»»»»»»»»»
+//  traversal
+
+// Perform pre-order traversal
+//
+void traverse_pre_order(node_t *node)
+{
+  if(node != NULL)
+    {
+      printf("%d ", node->key);
+      traverse_pre_order(node->left);
+      traverse_pre_order(node->right);
+    }
+}
+
+
+// Perform in-order traversal
+//
+void traverse_in_order(node_t *node)
+{
+  if(node != NULL)
+    {      
+      traverse_in_order(node->left);
+      printf("%d ", node->key);
+      traverse_in_order(node->right);
+    }
+}
+
+
+// Perform in-order traversal
+//
+void traverse_post_order(node_t *node)
+{
+  if(node != NULL)
+    {      
+      traverse_post_order(node->left);
+      traverse_post_order(node->right);
+      printf("%d ", node->key);      
+    }
+}
+
+
+/*
+ *  ------------------------------------
+ *  ------------------------------------
+ */ 
+
+
+int main( int argc, char **argv )
+
+ #define RECURSIVE 0
+ #define ITERATIVE 1
+  
+ #define PRE_ORDER 0
+ #define IN_ORDER 1
+ #define POST_ORDER 2
+{
+  int insertion = (argc > 1? atoi(*(argv+1)) : RECURSIVE );
+  int order     = (argc > 2? atoi(*(argv+2)) : PRE_ORDER );
+  node_t *root = NULL;
+  //
+  // build the tree
+
+  if ( insertion == RECURSIVE )
+    {
+      root = insert_recursive( root, 10 );
+      root = insert_recursive( root, 50 );
+      root = insert_recursive( root, 20 );
+      root = insert_recursive( root, 17 );
+      root = insert_recursive( root, 87 );
+      root = insert_recursive( root, 43 );
+      root = insert_recursive( root, 6 );
+      root = insert_recursive( root, 69 );
+      root = insert_recursive( root, 55 );
+    }
+  else
+    {
+  
+      insert( &root, 10 );
+      insert( &root, 50 );
+      insert( &root, 20 );
+      insert( &root, 17 );
+      insert( &root, 87 );
+      insert( &root, 43 );
+      insert( &root, 6 );
+      insert( &root, 69 );
+      insert( &root, 55 );
+    }
+  
+  //
+  
+  
+  if ( order == PRE_ORDER )
+    traverse_pre_order(root);
+  else if ( order == IN_ORDER )
+    traverse_in_order(root);
+  else if ( order == POST_ORDER )
+      traverse_post_order(root);
+
+  printf("\n");
+  return 0;
+}
---
+++ OpenMP/codes/linked_list.traverse.b.c
@@ -0,0 +1,806 @@
+
+#if defined(__STDC__)
+#  if (__STDC_VERSION__ >= 199901L)
+#     define _XOPEN_SOURCE 700
+#  endif
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+#include <omp.h>
+
+
+// =========================================================================
+//
+//  definees
+//
+
+
+// ··········································.
+//  types
+
+typedef unsigned           int uint;
+typedef unsigned long long int ull;
+
+
+
+// ··········································.
+//  timing
+//
+
+#if defined(_OPENMP)
+
+// get the wall-clock time
+#define CPU_TIME ({ struct timespec ts; (clock_gettime( CLOCK_REALTIME, &ts ), \
+					 (ull)ts.tv_sec * 1000000000 +	\
+					 (ull)ts.tv_nsec); })
+
+// get per-thread cpu time
+#define CPU_TIME_th ({ struct  timespec ts; (clock_gettime( CLOCK_THREAD_CPUTIME_ID, &ts ), \
+					     (ull)ts.tv_sec*1000000000 + \
+					     (ull)ts.tv_nsec); })
+
+#else
+
+// get per-proc cpu time
+#define CPU_TIME ({ struct timespec ts; (clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &ts ), \
+					 (ull)ts.tv_sec * 1000000000 +	\
+					 (ull)ts.tv_nsec); })
+#endif
+
+// ··········································.
+// get a timestamp for debugging purposes
+//
+#if defined(DEBUG)
+#define TIME_CUT 100000000
+#define TIMESTAP (CPU_TIME % TIME_CUT)
+#define dbgout(...) printf( __VA_ARGS__ );
+#else
+#define TIMESTAP
+#define dbgout(...) 
+#endif
+
+
+// ··········································.
+//  defines values for the
+//  case to run
+//
+#define FIRST_CASE     0
+#define FOR_STATIC     FIRST_CASE
+#define FOR_DYNAMIC    1
+#define USE_TASKS      2
+#define LAST_CASE      3
+
+char *case_names[LAST_CASE] =  {"running static loop over array"
+				"running dynamic loop over array"
+				"running with tasks"};
+
+// ··········································.
+//  defines the values for the various
+//  options in the walk routines
+//
+#define WALK_CHECK              0           // walk the list and check the sorting
+#define WALK_COUNT_ACTIVENODES  1           // count the active nodes
+#define WALK_INSERT_ACTIVENODES 2           // insert active nodes into an array
+#define WALK_GENERATE_TASKS     3           // generate tasks to process active nodes
+#define WALK_GENERATE_TASKSGROUPS 4
+#define WALK_VERIFY 5                       // verify that all active nodes have been
+					    // correctly processed
+
+//
+// =========================================================================
+//
+// define data structures
+//
+
+
+typedef struct llnode
+{
+  uint data;
+  uint result;
+  struct llnode *next;
+  struct llnode *prev;  
+} llnode_t;
+
+
+
+//// =========================================================================
+//
+// declare global data
+
+uint  howmanynodes= 0;                       // how many nodes have been processed, fir every thread
+ull   howmuchtime;                           // how much time has been spent in processing nodes, per thread
+#pragma omp threadprivate(howmanynodes, howmuchtime)
+
+char  filename_dflt[] = "nodes.dat";
+uint  ActiveThreshold;
+uint  MaxLimit;
+
+//
+// =========================================================================
+//
+// prototypes
+//
+
+
+inline llnode_t *create_node         ( uint, llnode_t *, llnode_t * );
+uint      load_nodes_and_build_list  ( llnode_t **, char*, int *, uint *, uint * );
+       uint      find                ( llnode_t *, uint, llnode_t ** restrict, llnode_t ** restrict );
+       uint      find_and_insert     ( llnode_t **, uint );
+
+inline int       is_active           ( uint );
+void      process_nodes_group ( llnode_t * restrict * restrict, uint);
+ull       process_node        ( llnode_t * restrict, uint );
+
+       llnode_t *get_head            ( llnode_t * restrict );
+       int       delete              ( llnode_t * restrict );
+       uint      walk                ( llnode_t * restrict, int, void * restrict );
+
+
+//
+// =========================================================================
+// =========================================================================
+
+
+int is_active( uint data  )
+{
+  return (data >= ActiveThreshold);
+}
+
+void process_nodes_group( llnode_t * restrict list[], uint maxlimit )
+{
+  ull timing = CPU_TIME_th;
+  // ·························
+  
+  int idx    = 0;  
+  while ( list[idx] != NULL )
+    {
+      llnode_t *node  = (llnode_t*)list[idx];
+      ull       wow   = 0;
+      ull       limit = node->data;
+      limit = (limit > maxlimit ? maxlimit : limit);
+ 
+      for ( uint i = 0; i < limit ; i++ )
+	wow += i;;
+      ((llnode_t*)node)->result = wow;
+      howmanynodes++;
+
+      idx++;
+    }
+
+  // ·························
+  howmuchtime += CPU_TIME_th - timing;
+  return;
+}
+
+
+ull process_node( llnode_t * restrict node, uint maxlimit )
+{
+  ull timing = CPU_TIME_th;
+  // ·························
+
+  ull wow   = 0;
+  ull limit = node->data;
+  limit = (limit > maxlimit ? maxlimit : limit);
+  
+  for ( uint i = 0; i < limit ; i++ )
+    wow += i;
+  ((llnode_t*)node)->result = wow;
+  howmanynodes++;
+  
+  // ·························
+  howmuchtime += CPU_TIME_th - timing;  
+  return wow;
+}
+
+
+// ······················································
+
+llnode_t *get_head ( llnode_t *start )
+/*
+ * walk the list basck to find the list head
+ * returns the head
+ */
+{
+  llnode_t *walk = start;
+  while( walk->prev != NULL )
+    walk = walk->prev;
+  
+  return walk;
+}
+
+
+// ······················································
+
+int delete ( llnode_t *head )
+/*
+ * delete all the nodes
+ */
+{
+  head = get_head(head);
+  while ( head != NULL )
+    {
+      llnode_t *prev = head;
+      head = head->next;
+      free( prev );
+    }
+  return 0;
+}
+
+// ······················································
+
+uint walk ( llnode_t *start, int mode, void *pointer )
+/*
+ * walk the list starting from the node start
+ * as first, the list is walked back until the list head
+ * if mode == 1, the list is then walked ahed printing
+ * the first 100 nodes.
+ */
+{
+  uint ncount  = 0;
+  uint _count_ = 0;
+
+  void **array = (void**)pointer;
+  uint  *count = (uint*)pointer;
+  uint   chunk_size;
+
+  if ( mode == WALK_GENERATE_TASKSGROUPS )
+    chunk_size = *(uint*)pointer;
+  else
+    chunk_size = 1;
+  
+  if ( start != NULL )
+    {
+      
+      llnode_t *walk = start;
+      llnode_t *list[chunk_size+1];
+      for ( int i = 0; i <= chunk_size; i++ ) list[i] = NULL;
+      
+      int  list_size = 0;
+      uint prev_value = (walk != NULL ? walk->data : 0);
+      
+      while( walk != NULL)
+	{
+	  switch ( mode )
+	    {
+
+	      // check that the nodes are sorted
+	      //
+	    case WALK_CHECK : {
+	      _count_++;
+	      ncount += (walk->data < prev_value);
+	      prev_value = walk->data; } break;
+	      
+	      // count how many nodes have to be processed
+	      //
+	    case WALK_COUNT_ACTIVENODES: 
+	      ncount += is_active ( walk->data );
+	      break;
+	      
+	      // insert active nodes into an array
+	      //
+	    case WALK_INSERT_ACTIVENODES: {
+	      if ( is_active ( walk->data ) )
+		array[ncount++] = (void*)walk; }
+	      break;
+
+	      // generate a task per every node
+	      //
+	    case WALK_GENERATE_TASKS: {
+	      if ( is_active ( walk->data ) )
+		ncount++;
+	     #pragma omp task firstprivate(walk) untied
+	      process_node( walk, MaxLimit ); };
+	      break;
+
+	      // generate a task every TASKGROUP_SIZE nodes
+	      //
+	    case WALK_GENERATE_TASKSGROUPS: {
+	      if ( is_active ( walk->data ) )
+		{
+		  ncount++;
+		  list[list_size++] = walk;
+		  if ( list_size == chunk_size ) {
+		   #pragma omp task firstprivate(walk, list) untied
+		    process_nodes_group( list, MaxLimit );
+		    memset( list, 0, sizeof(void*)*chunk_size);
+		    list_size = 0; }
+		} }
+	      break;
+
+	      // verify that all the active nodes have been processes
+	      // and that the results are correct
+	    case WALK_VERIFY: {
+	      // count how many active nodes have been processed
+	      _count_ += (walk->result > 0); 
+	      if ( is_active( walk->data ) )
+		{		  
+		  ull wow   = walk->data * (walk->data-1) / 2;
+		  ncount += (wow != walk->result);
+		} }
+	      break;
+	      
+	    }
+	  
+	  walk = walk->next;
+	}
+      
+      if ( (mode == WALK_GENERATE_TASKSGROUPS) && (list_size > 0 ) )
+       #pragma omp task firstprivate(walk, list) untied
+	process_nodes_group( list, MaxLimit );
+
+    }
+      
+  if ( ((mode == WALK_CHECK)||(mode == WALK_VERIFY)) && (count != NULL ) )
+    *count = _count_;
+  return ncount;
+}
+
+
+
+// ······················································
+
+uint find ( llnode_t *head, uint value, llnode_t **prev, llnode_t **next )
+{
+  *prev = NULL, *next = NULL;
+  
+  if ( head == NULL )
+    // The first node must exist in this simple
+    // implementation.
+    // To improve that, pass **head instead
+    // of *head
+    return -1;
+
+  uint       nsteps = 0;
+  llnode_t  *ptr = NULL;
+
+  if ( head-> data > value )
+    {
+      // we need to walk back
+      //
+      ptr  = head->prev;
+      *next = head;
+      while ( (ptr != NULL) && (ptr->data > value) )
+	{
+	  *next = ptr;
+	  ptr  = ptr->prev;
+	  nsteps++;
+	}
+      *prev = ptr;
+    }
+  else
+    {
+      // we need to walk ahead
+      //
+      ptr  = head->next;
+      *prev = head;
+      while ( (ptr != NULL) && (ptr->data < value) )
+	{
+	  *prev = ptr;
+	  ptr  = ptr->next;
+	  nsteps++;
+	}
+      *next = ptr;
+    }
+
+  return nsteps;
+}
+
+
+// ······················································
+llnode_t * create_node ( uint data, llnode_t *prev, llnode_t *next )
+{
+  llnode_t *node = (llnode_t*)malloc( sizeof(llnode_t) );
+  if ( node != NULL )
+    {
+      node-> data   = data;
+      node-> result = 0;
+      node-> prev   = prev;
+      node-> next   = next;
+    }
+  return node;
+}
+
+
+// ······················································
+uint find_and_insert( llnode_t **head, uint value )
+{
+  if ( *head == NULL )
+    {
+      *head = create_node( value, NULL, NULL );
+      if ( *head == NULL )
+	return -2;
+    }
+  else
+    {
+      llnode_t *prev = NULL, *next = NULL;
+
+      find ( *head, value, &prev, &next );
+  
+      llnode_t *new = create_node( value, prev, next);
+
+      if ( new != NULL )
+	{
+	  if( prev != NULL )
+	    prev->next = new;
+	  if( next != NULL )
+	    next->prev = new;
+	}
+      else
+	return -2;
+    }
+  
+  return 0;
+}
+
+
+uint load_nodes ( char *filename,
+		  int *mode, uint *cutoff, uint *range,
+		  uint **nodes)
+{
+
+  FILE *file = fopen( filename, "r" );
+  if ( file == NULL ) {
+    printf ( "unable to open the file %s\n", filename );
+    *mode = 1;
+    return 0; }
+
+  uint    N;
+  size_t  ret;
+  
+  // ···········································
+  // load data from the file
+  // 
+  ret = fread ( mode, sizeof(uint), 1, file );
+  ret = fread ( &N, sizeof(uint), 1, file );
+  ret = fread ( cutoff, sizeof(uint), 1, file );
+  if ( *mode == 1 )
+    ret = fread( range, sizeof(uint), 1, file );
+  else
+    *range = 0;
+
+  *nodes = (uint*) malloc( sizeof(uint)*N );
+  ret    = fread ( *nodes, sizeof(uint), N, file );
+  fclose(file);
+  //
+  // ···········································
+  
+  return N;
+}
+  
+
+
+
+
+/*
+
+   ············································································································
+   ············································································································
+   ············································································································
+
+*/
+
+
+int main ( int argc, char **argv )
+{
+  
+  // options controlling how the nodes are generated
+  //
+  uint  N;               // how many nodes to generate
+    
+  long int seed;         // if needed
+  
+  
+  // options controlling the case to run
+  int   case_to_run;     // 1: static for-loop over an array
+                         // 2: dynamic for-loop over array
+                         // 3: tasks
+                         
+  int   chunk;           // more details on the case to run
+			 // for loop: the size of the chunks for the scheduling,
+			 //           0 means do not specify a value
+			 // tasks:    the groupsize of the task (i.e. how many
+			 //           nodes are included in a task)
+
+  float fraction;
+  
+  char     *filename;
+  llnode_t *head = NULL;
+  ull       timing;
+  
+
+  {
+    // parse the command line arguments
+    // let's use getopt since we use a simple command line
+  
+    // set-up the default
+    N              = 0;
+    case_to_run    = 0;
+    chunk          = 1;
+    MaxLimit       = 0;
+    fraction       = 0.6;
+    filename       = filename_dflt;
+
+    int const_value = 0;
+    int c;
+    
+    while ( (c = getopt(argc, argv, "N:c:f:C:s:F:v:h"))  != -1 )
+      switch ( c ) {
+
+      case 'N':
+	N = atoi(optarg);
+	break;
+	
+      case 'c':
+	case_to_run = atoi(optarg);
+	break;
+
+      case 'f':
+	int len = strlen(optarg);
+	filename = (char*)malloc( len+2 );
+	snprintf( filename, len+1, "%s", optarg );
+	break;
+
+      case 'M':
+	MaxLimit = (uint)atoi( optarg );
+	break;
+	
+      case 'v':
+	const_value = (uint)atoi( optarg );
+	break;
+	
+      case 'C':
+	chunk = (uint)atoi(optarg);
+	break;
+
+      case 'F':
+	fraction = (float)atof(optarg);
+	break;
+
+      case 's':
+	seed = atoi(optarg);
+	break;
+
+      case 'h':
+	printf("\npossible arguments: "
+	       "-c _ -C _ -F _ -M _ -f _ -v _ -N _ -s _\n\n"
+	       "\tc :  0 -> for loop with static schedule\n"
+	       "\t     1 -> for loop with dynamic schedule\n"
+	       "\t     2 -> tasks\n"
+	       "\tC :  the chunk size for case 0,1 or the bunch size for case 2\n"
+	       "\tF :  the fraction of nodes that will be \"active\"\n"
+	       "\tM :  the max value to be used when processing a node\n"
+	       "\tf :  the name of the file that contains the nodes' values\n"
+	       "\tv :  the value to be used to generate a nu,ber of nodes with constant value\n"
+	       "\tN :  the number of nodes to be generated with constant value\n" );
+	exit( 0 );
+	break;
+	
+      default:
+	break;
+	
+      }
+
+    if ( (case_to_run < FIRST_CASE) || (case_to_run >= LAST_CASE) ) {
+      printf ( "unknown case %d\n", case_to_run );
+      exit(1); }
+
+    if ( ( (const_value != 0) ||
+	   (N > 0) ) &&
+	 (const_value*N == 0) )
+      {
+	printf ("if you want to generate nodes with constant values "
+		"you must specify both the values using -N and -v options\n");
+	return -1;
+      }
+    
+    if ( (const_value > 0)  &&
+	 (filename != filename_dflt) )
+      {
+	printf ( " you can not require to load data from file %s "
+		 "and at the same time to generate %u nodes "
+		 "with constant value %u\n",
+		 filename, N, const_value );
+	return -2;
+      }
+    
+    if ( const_value == 0 )
+      {
+	uint *nodes = NULL;
+	int   mode;
+	uint  cutoff;
+	uint  range;
+
+	printf ( " > loading the values.. " );
+	timing = CPU_TIME;
+	N = load_nodes( filename, &mode, &cutoff, &range, &nodes );
+	timing = CPU_TIME - timing;
+	printf ( "took %g seconds\n", timing/1e9);
+	
+	if ( N == 0 )
+	  exit(mode);
+    
+	if ( mode == 0 )
+	  ActiveThreshold = (1-fraction)*cutoff;
+	else if ( mode == 1 )
+	  ActiveThreshold = cutoff-range + 2*range*(1-fraction);
+
+	if ( MaxLimit == 0 )
+	  MaxLimit = cutoff;
+	
+	printf ( " > building the nodes.. " );
+	timing = CPU_TIME;
+	uint i_10 = N/10;
+	uint i;
+	for ( i = 0; i < N; i++ )
+	  {
+	    int res = find_and_insert( &head, nodes[i] );
+	    if ( res < 0 )
+	      break;
+	    if ( i % i_10 == 0 )
+	      printf("%u%%.. ", 10*i/i_10);
+	  }
+	timing = CPU_TIME - timing;
+	printf ( "took %g seconds\n", timing/1e9);	
+	free ( nodes );
+	if ( i < N )
+	  {
+	    printf("unable to allocate room for "
+		   "%u nodes (limit was at %u)\n",
+		   N, i );
+	    delete ( head );
+	    exit( -1 );
+	  }
+      }
+    else
+      {
+	ActiveThreshold = const_value*0.99;
+	if ( MaxLimit == 0 )
+	  MaxLimit = const_value;
+
+	uint i;
+	for ( i = 0; i < N; i++ )
+	  {
+	    int res = find_and_insert( &head, const_value );
+	    if ( res < 0 )
+	      break;
+	  }
+	if ( i < N )
+	  {
+	    printf("unable to allocate room for "
+		   "%u nodes (limit was at %u)\n",
+		   N, i );
+	    delete ( head );
+	    exit( -1 );
+	  }	
+      }
+
+    head = get_head( head );
+  }
+
+  
+  
+  printf(" > checking the list.. "); fflush(stdout);
+  timing = CPU_TIME;
+
+  uint actual_nodes;
+  uint faults = walk( head, WALK_CHECK, &actual_nodes);
+
+  timing = CPU_TIME - timing;
+  printf ( "took %g seconds\n", (double)timing/1e9 );  
+
+  if ( actual_nodes != N )
+    printf("shame on me! %u nodes instead of %u have been found!",
+	   actual_nodes, N);
+  if ( faults > 0 )
+    printf("a tragedy occured.. %u nodes are unsorted!!\n", faults );
+
+  printf(" > counting active nodes.. "); fflush(stdout);
+  timing = CPU_TIME;
+  
+  uint Nactive = walk( head, WALK_COUNT_ACTIVENODES, NULL );
+
+  timing = CPU_TIME - timing;
+  printf ( "%u active nodes found (took %g seconds)\n", Nactive, (double)timing/1e9 );
+
+  if ( Nactive < 0 )
+    {
+      delete ( head );
+      exit(0);
+    }
+
+  printf(" >>  now processing\n");
+  ull alltime = CPU_TIME;
+   
+  if ( case_to_run < USE_TASKS )
+    {
+      // build an array of pointers to nodes and process it using an omp for loop
+
+      void **array;
+      array = (void**)calloc( Nactive, sizeof(void*) );
+      if ( array == NULL )
+	{
+	  printf("not enough memory to host %u pointers to active nodes\n", Nactive );
+	  delete(head);
+	  exit(1);
+	}
+
+      printf( "\t * preparing the array.. "); fflush(stdout);
+      timing = CPU_TIME;
+      walk( head, WALK_INSERT_ACTIVENODES, (void*)array );
+      timing = CPU_TIME - timing;
+      printf ( "took %g seconds\n", (double)timing/1e9 );
+      
+      printf ( "\t * processing the active nodes.. "); fflush(stdout);
+
+      timing = CPU_TIME;
+      if ( case_to_run == FOR_STATIC )
+	{
+	 #pragma omp parallel for schedule(static, chunk)
+	  for ( int i = 0; i < Nactive; i++ )
+	    process_node( array[i], MaxLimit );
+
+	}
+      else if ( case_to_run == FOR_DYNAMIC )
+	{
+	 #pragma omp parallel for schedule(dynamic, chunk)
+	  for ( int i = 0; i < Nactive; i++ )
+	    process_node( array[i], MaxLimit );	      
+	}
+      timing = CPU_TIME - timing;
+      printf ( "took %g seconds\n", (double)timing/1e9 );
+
+      free ( array );
+    }
+
+  else 
+    {
+      // use tasks
+      
+      printf( "\t * generating the tasks and processing the array.. "); fflush(stdout);
+      timing = CPU_TIME;
+     #pragma omp parallel
+      {
+	int mode = (chunk > 1 ? WALK_GENERATE_TASKSGROUPS : WALK_GENERATE_TASKS );
+       #pragma omp single
+	{
+	 #pragma omp task untied
+	  walk( head, mode, &chunk );
+	}
+      }
+      timing = CPU_TIME - timing;
+      printf ( "took %g seconds\n", (double)timing/1e9 );
+      
+    }
+
+  alltime = CPU_TIME - alltime;
+  printf ( "\ntotal time: %g seconds\n", (double)alltime / 1e9 );
+  
+ #pragma omp parallel
+  {
+    int nthreads = omp_get_num_threads();
+   #pragma omp for ordered
+    for ( int i = 0; i < nthreads; i++ )
+     #pragma omp ordered
+      printf(" - thread %d processed %u nodes in %g seconds\n", i, howmanynodes, howmuchtime/1e9 );
+    
+    //free(myresults);
+  }
+  
+  uint verified;
+  faults = walk( head, WALK_VERIFY, &verified );
+  if ( faults )
+    printf( "%u faults found!\n", faults );
+  if ( Nactive != verified )
+    printf( "%u verified instead of %u\n", verified, Nactive );
+  
+  delete ( head );
+
+  
+ #undef USE_ARRAY
+  return 0;
+}
