43c3804367762bbd6e3dcc667a327b9b87232114
luca <luca.tornatore@inaf.it>
2025-11-11 13:43:26 +0100

    .

--- Vectorization/99_sparse_examples/branch_to_mask.c
+++ Vectorization/99_sparse_examples/branch_to_mask.c
@@ -14,7 +14,7 @@ void transform_branch(float* a, const float* b, int n, float t)
 void transform_mask(float* a, const float* b, int n, float t)
 {
  #pragma omp simd
-  for(int i=0;i<n;i++)
+  for(int i = 0; i < n; i++)
     {
       float ai = a[i], bi = b[i];
       a[i] = (ai > t) ? (bi - ai) : ai;
@@ -28,7 +28,7 @@ int main(int argc, char**argv)
   float* b = (float*)aligned_alloc(64, n*sizeof(float));
   
   for ( int i = 0; i < n; i++)
-    { a[i]=i*0.1f; b[i]=i*0.2f; }
+    { a[i] = i*0.1f; b[i] = i*0.2f; }
   
     transform_branch(a,b,n,50.0f);
     transform_mask(a,b,n,50.0f);
