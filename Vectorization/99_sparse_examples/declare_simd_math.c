5c91cd8304ba4e823d635ddd21bb06975814a545
luca <luca.tornatore@inaf.it>
2025-11-21 00:14:06 +0100

    update

--- Vectorization/99_sparse_examples/declare_simd_math.c
+++ Vectorization/99_sparse_examples/declare_simd_math.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../headers/vector_pragmas.h"
+#include "../headers/timing.h"
+
 #pragma omp declare simd notinbranch simdlen(8) uniform(b)
 double f(double a, double b){
     return sin(a) + a*b;
@@ -11,8 +14,8 @@ double f(double a, double b){
 
 double loop_plain(double *x, double b, int n)
 {
-  double s=0.0;
-  for(int i=0;i<n;i++)
+  double s = 0.0;
+  for (int i = 0; i < n; i++)
     s += f(x[i], b);
   return s;
 }
@@ -21,7 +24,8 @@ double loop_simd(double *x, double b, int n)
 {
   double s=0.0;
  #pragma omp simd reduction(+:s)
-  for(int i=0;i<n;i++) s += f(x[i], b);
+  for (int i = 0; i < n; i++)
+    s += f(x[i], b);
   return s;
 }
 
@@ -32,9 +36,19 @@ int main(int argc, char**argv)
   
   for(int i=0;i<n;i++)
     x[i] = i*0.001;
+
+
+  double s, t;
   
-  printf("plain: %f\n", loop_plain(x,b,n));
-  printf("simd : %f\n", loop_simd(x,b,n));
+  t = PCPU_TIME;
+  s = loop_plain(x,b,n);
+  t = PCPU_TIME - t;
+  printf ("%g\n", s );
+
+  t = PCPU_TIME;
+  s = loop_simd(x,b,n);
+  t = PCPU_TIME - t;
+  printf ("%g\n", s );
   
   free(x);
 
